9988f9f5264b9710149406838e8dedf4b59c1734
kawtarzz <kawtaryazzouzi@gmail.com>
2023-10-24 17:09:22 -0500

    format on portfolio, small css changes on layouts

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,52 +1,48 @@
 import React from "react";
 import { projects } from "./Projects/data.js"
-import "./index.scss"
+import { Card } from "react-bootstrap";
 
 function Portfolio() {
   return (
-    <div className="portfolio-page">
-      <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-        <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-          <h1 className="page-title">
+    <>
+      <div className="container portfolio-page">
+        <div className="text-zone">
+          <h1>
             Portfolio
           </h1>
-        </div>
-        <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
-            <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="images-container">
-                <div className="image-box">
-                  <img
-                    alt="gallery"
-                    className="absolute inset-0 w-full h-full object-cover object-center"
-                    src={project.image}
-                  />
-                </div>
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+            <Card key={project.id}>
+              <Card.Body>
+                <Card.Title>
+                  <h2>
                     {project.title}
-                  </h1>
-                  <hr></hr>
-                  <h4>{project.description}</h4>
-                  <div className="flex justify-center mt-4">
-
-                    <h3 className="tracking-widest text-sm title-font font-medium text-green-500 mb-1">
-                      Tech:
-                    </h3>
+                  </h2>
+                </Card.Title>
+                <Card.Img variant="top" className="header_img" src={project.image} to={project.link} />
+                <hr></hr>
+                <Card.Text>
+                  {project.description}
+                  <Card.Subtitle>
                     <h4>
-                      {project.subtitle}
+                      Tech:
                     </h4>
-                  </div>
-                </div>
-              </div>
-            </a>
+                    <h5>
+                      {project.subtitle}
+                    </h5>
+                    <p>
+
+                      <Card.Link href={project.repolink}>Repo</Card.Link>
+                    </p>
+                  </Card.Subtitle>
+
+                </Card.Text>
+              </Card.Body>
+            </Card>
           ))}
         </div>
-      </section>
-    </div>
+      </div>
+      <hr />
+    </>
   );
 }
 
